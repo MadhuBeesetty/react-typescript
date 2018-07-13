4800a3a9ec7aa2dd45419b7763d6614ac3f0a8c5
Madhu Beesetty <mbeesetty@sfml-mbeesetty.chegg.com>
2018-07-13 16:24:32 -0700

    adding child

---
+++ react-tyescript/src/components/child1.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Child1 extends React.Component {
+  public render() {
+    return (
+      <div>
+       <h1>winter is coming</h1>
+      </div>
+    );
+  }
+}
+
+export default Child1;
